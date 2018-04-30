a2029763b264c51e5c5ebac98f50f9ab546c6624
Stefano <37554784+stefanocirici@users.noreply.github.com>
2018-04-30 10:17:41 +0200

    Revert gpio changes

--- rosberry/entrypoint.sh
+++ rosberry/entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # change the owner and group of /dev/mem to root and gpio respectively.
-chown root.gpio /dev/mem
+#chown root.gpio /dev/mem
 # gives the group read write access to this /dev/mem object.
-chmod g+rw /dev/mem
+#chmod g+rw /dev/mem
 
 #call blinking led sccript
 python /blink_led.py & BLINK_ID=$!
